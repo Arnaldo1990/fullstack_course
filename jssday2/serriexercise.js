9fd22614540554be92126dab34812ca32b1d300d
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-02 11:20:00 +0100

    changes

---
+++ jssday2/serriexercise.js
@@ -0,0 +1,10 @@
+let temp = 1;
+if (temp < 2) {
+    console.log("Very Cold");
+} else if (temp < 9) {
+    console.log("Cold");
+} else if (temp < 20) {
+    console.log("ok");
+} else {
+    console.log("Hot");
+}
